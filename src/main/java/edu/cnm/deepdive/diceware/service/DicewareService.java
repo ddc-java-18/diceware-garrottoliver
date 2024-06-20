cd380f27bdee0e86eac9f3de881c96f27fe2bb86
oliver <ogarrott@cnm.edu>
2024-06-20 11:30:54 -0600

    Started implementation of Diceware

---
+++ src/main/java/edu/cnm/deepdive/diceware/service/DicewareService.java
@@ -0,0 +1,35 @@
+package edu.cnm.deepdive.diceware.service;
+
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+import java.util.stream.IntStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DicewareService implements
+    PassphraseService {
+
+  private final List<String> words;
+  private final RandomGenerator rng;
+
+  @Autowired
+  public DicewareService(WordsProvider provider, RandomGenerator rng) {
+    this.words = new ArrayList<>(provider.words());
+    Collections.shuffle(words, rng);
+    this.rng = rng;
+  }
+
+  @Override
+  public List<String> generate(int length) {
+    return IntStream.generate(() -> rng.nextInt(words.size()))
+        .limit(length)
+        .mapToObj(words::get)
+    .toList();
+  }
+}
